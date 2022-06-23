9c9a8cb6b5c4ba440e6146f67cd6acaac30aab2b
sandren <20441876+sandren@users.noreply.github.com>
2022-06-23 15:44:52 -0700

    updates

--- framer-motion/components/reveal.js
+++ framer-motion/components/reveal.js
@@ -23,11 +23,11 @@ export const Reveal = ({ className = "", children, ...rest }) => {
 
 const variants = {
   hidden: {
-    y: 200,
+    transform: "translate3d(0, 200px, 0)",
     opacity: 0,
   },
   visible: {
-    y: 0,
+    transform: "translate3d(0, 0px, 0)",
     opacity: 1,
   },
 };
